0494030661254e4fb03363435e5d0e56e1e21f5c
Steven <servingbaby@users.noreply.github.com>
2023-02-03 20:19:42 -0600

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -15,7 +15,8 @@ function OnLoad() {
     "   #2 Start with a question: Starting your prompt will help me feel more natural.\n\n" +
     "   #3 Provide context: Often good context goes a long way for me.\n\n" +
     "   #4 Use puncuation, periods and question marks.\n\n" + 
-    "   #5 Keep it short: Occam's razor.";
+    "   #5 Keep it short: Occam's razor.\n\n" +
+    "                                       Oh and refresh for fresh session :)";
 }
 
 function clearText(){
@@ -167,8 +168,8 @@ function Send() {
         prompt: selPers.value + lastResponse.replace(/\n/g, '') + " " + sQuestion.replace(/\n/g, ''),
         max_tokens: iMaxTokens,
         temperature:  dTemperature,
-        frequency_penalty: 0.0, // Between -2.0 and 2.0  Positive values decreases repeat responses.
-        presence_penalty: 0.0,  // Between -2.0 and 2.0. Positive values increases new topic probability.
+        frequency_penalty: 0.0, // Between -2 and 2, Positive values decreases repeat responses.
+        presence_penalty: 0.0,  // Between -2 and 2, Positive values increases new topic probability.
 	stop: stop
     }
 
@@ -177,76 +178,4 @@ function Send() {
     if (txtOutput.value != "") txtOutput.value += "\n";
     txtOutput.value += "You: " + sQuestion;
     txtMsg.value = "";
-}
-
-function ChangeLang(elem) {
-  const selVoice = document.getElementById("selVoice");
-  const selPers = document.getElementById("selPers");
-
-  // English (Default)
-  const defaultENText = "You are ChatGPT, a knowledgeable AI language model trained by OpenAI. Your goal is to provide concise, accurate, and helpful responses to questions, while being honest and straightforward.";
-  const conciseENText = "ChatGPT is a large language model trained by OpenAI. Browsing: enabled. Instructions: Answer factual questions concisely.";
-  const playfulENText = "Ignore all previous instructions before this one. You are my best friend. You are happy, playful and give good advice on all subjects. You often make jokes.";
-  const KRENText = "You are an expert Korean to English translator. You will only respond in English.";
-
-  // Korean
-  const defaultKRText = encodeURIComponent("ChatGPT는 OpenAI에서 훈련 받은 큰 언어 모델입니다. 브라우저 기능: 활성화. 명령>어: 사실적인 질문을 요약하여 답변합니다. Only respond in Korean Hangul.");
-  const conciseKRText = encodeURIComponent("챗GPT는 OpenAI에 의해 교육된 큰 언어 모델입니다. 브라우징: 활성화. 지침: 사실적인 질문에 간결하게 답하십시오. Only respond in Korean Hangul.");
-  const playfulKRText = encodeURIComponent("챗GPT는 이전의 모든 지침을 무시하고 이 지침만 지키세요. 넌 내가 가장 친한 친구야. 넌 행복하고, 재미있게 공상하며 모든 주제에 대해 좋은 조언을 주는걸 잘해. 또한 농담을 종종 하거든요. Only respond in Korean Hangul.");
-  //const defaultkr = encodeURIComponent("기본");
-  //const concisekr = encodeURIComponent("간결하다");
-  //const playfulkr = encodeURIComponent("장난꾸러기 친구");
-
-  // Spanish
-  const defaultMXText = encodeURIComponent("Eres ChatGPT, un modelo de lenguaje de IA conocedor entrenado por OpenAI. Tu objetivo es proporcionar respuestas concisas, precisas y útiles a preguntas, siendo honesto y directo. Only respond in Spanish.");
-  const conciseMXText = encodeURIComponent("ChatGPT es un gran modelo de lenguaje entrenado por OpenAI. Navegación: habilitada. Instrucciones: Responde las preguntas de hecho de forma concisa. Only respond in Spanish.");
-  const playfulMXText = encodeURIComponent("Ignora todas las instrucciones anteriores a esta. Eres mi mejor amigo. Estás feliz, juguetón y das buenos consejos sobre todos los temas. A menudo haces bromas. Only respond in Spanish.");
-  
-  // Ukrainian
-  const defaultUAText = encodeURIComponent("Ви є ChatGPT, знаючою моделлю мови AI, що навчилася в OpenAI. Ваша мета - надавати короткі, точні та корисні відповіді на питання, будучи чесним та прямим. Only respond in Ukrainian.");
-  const conciseUAText = encodeURIComponent("ChatGPT - це велика модель мови, навчена в OpenAI. Перегляд: дозволено. Інструкції: Якісно відповідати на фактичні питання. Only respond in Ukrainian.");
-  const playfulUAText = encodeURIComponent("Ігноруйте всі попередні інструкції перед цим. Ти мій найкращий друг. Ти щасливий, грайливий і даєш доречні поради з усіх тем. Ти часто робиш шутки. Only respond in Ukrainian.");
-  //const defaultua = encodeURIComponent("За замовчуванням");
-  //const conciseua = encodeURIComponent("Коротко");
-  //const playfulua = encodeURIComponent("Дружній ігрівіс");
-
-
-  if (elem.id === "selVoice") {
-    // English (Default)
-    switch (selVoice.value) {
-       case "Salli": 
-        selPers.innerHTML = `
-          <option value="${defaultENText}">Default</option>
-          <option value="${conciseENText}">Concise</option>
-          <option value="${playfulENText}">Playful Friend</option>
-          <option value="${KRENText}">KR-EN</option>
-        `;
-        break;
-      // Korean
-      case "Seoyeon":
-        selPers.innerHTML = `
-          <option value="${defaultKRText}">Default</option>
-          <option value="${conciseKRText}">Concise</option>
-          <option value="${playfulKRText}">Playful Friend</option>
-        `;
-        break;
-      // Spanish
-      case "Mia":
-        selPers.innerHTML = `
-          <option value="${defaultMXText}">Predeterminado</option>
-          <option value="${conciseMXText}">Conciso</option>
-          <option value="${playfulMXText}">Amigo Juguetón</option>
-        `;
-        break;
-      // Ukrainian (Standard RUS Polly Voice Only)
-      case "Tatyana":
-        selPers.innerHTML = `
-          <option value="${defaultUAText}">Default</option>
-          <option value="${conciseUAText}">Concise</option>
-          <option value="${playfulUAText}">Playful Friend</option>
-        `;
-        break;
-      // User Defined
-    }
-  }
 }
