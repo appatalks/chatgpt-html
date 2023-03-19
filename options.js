86ec4c20fc2787f613a35c712b55e64329d9f18f
Steven <servingbaby@users.noreply.github.com>
2023-03-18 23:41:09 -0500

    Update options.js

--- options.js
+++ options.js
@@ -8,8 +8,9 @@
 // Error Handling Variables
 var lastResponse = "";
 var userMasterResponse = "";
-// var aiMasterResponse = "";
+var aiMasterResponse = "";
 var masterOutput = "";
+var storageAssistant = "";
 var retryCount = 0;
 var maxRetries = 5;
 var retryDelay = 2420; // initial delay in milliseconds
