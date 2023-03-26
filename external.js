19741a8c3eb4cd0c9c8ea79c0e63338b3d5671c1
Steven <servingbaby@users.noreply.github.com>
2023-03-26 13:50:57 -0500

    Work in Progress

--- external.js
+++ external.js
@@ -1,15 +1,27 @@
+// Inital External Data Discovery
 const file = "external.data"; // relative path to the file
-
 let fileContents; // create a variable to store the file contents
 
 fetch(file)
   .then(response => response.text())
   .then(contents => {
     fileContents = contents.replace(/\n/g, ''); // store the file contents in the variable
-    console.log(fileContents);
+  //  console.log(fileContents);
     // do something with the file contents
   })
-  .catch(error => console.log(error));
 
-// the file contents can be accessed outside of the fetch() method
-console.log(fileContents);
+// Test KeyWord Template "doh"
+const doh = "doh.data"; // relative path to the file
+let dohContents; // create a variable to store the file contents
+ 
+ fetch(doh)
+   .then(response => response.text())
+   .then(contents => {
+     dohContents = contents.replace(/\n/g, ''); // store the file contents in the variable
+ //    console.log(dohContents);
+     // do something with the file contents
+   })
+
+//
+// Other Sources Template
+// Check for keyword/phrase in sQuestion
