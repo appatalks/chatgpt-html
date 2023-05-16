921068d55a7894e68776fa6031b6a29821338b83
Steven <servingbaby@users.noreply.github.com>
2023-05-16 16:24:22 -0500

    Update google-palm.js
    output formatting

--- google-palm.js
+++ google-palm.js
@@ -1,48 +1,55 @@
-// Javascript
-// For Google PaLM API ie Bard
-
 function palmSend() {
-var sQuestion = txtMsg.innerHTML;
 
-function auth() {
-  return fetch('./config.json')
-    .then(response => response.json())
-    .then(config => config.GOOGLE_PALM_KEY);
-}
+  function auth() {
+    return fetch('./config.json')
+      .then(response => response.json())
+      .then(config => config.GOOGLE_PALM_KEY);
+  }
 
-const MODEL_NAME = "chat-bison-001";
+  var sQuestion = document.getElementById("txtMsg").innerHTML;
+    sQuestion = sQuestion.replace(/<br>/g, "\n");
+  if (sQuestion.trim() == "") {
+    alert("Type in your question!");
+    txtMsg.focus();
+    return;
+  }
 
-auth().then(GOOGLE_PALM_KEY => {
-  const gapiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/${MODEL_NAME}:generateMessage?key=${GOOGLE_PALM_KEY}`;
+  const MODEL_NAME = "chat-bison-001";
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: {
-        context:
-          "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, and helpful responses to questions, while being honest and straightforward.",
-        examples: [],
-        messages: [{ content: sQuestion }],
+  auth().then(GOOGLE_PALM_KEY => {
+	txtMsg.innerHTML = "";
+	txtOutput.innerHTML += "You: " + sQuestion + "\n";
+    const gapiUrl = `https://generativelanguage.googleapis.com/v1beta2/models/${MODEL_NAME}:generateMessage?key=${GOOGLE_PALM_KEY}`;
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      temperature: 0.25,
-      top_k: 40,
-      top_p: 0.95,
-      candidate_count: 1,
-    }),
-  };
+      body: JSON.stringify({
+        prompt: {
+          context:
+            "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, and helpful responses to questions, while being honest and straightforward.",
+          examples: [],
+          messages: [{ content: sQuestion }],
+        },
+        temperature: 0.25,
+        top_k: 40,
+        top_p: 0.95,
+        candidate_count: 1,
+      }),
+    };
 
-  fetch(gapiUrl, requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(JSON.stringify(result, null, 2));
-	txtOutput.innerHTML += JSON.stringify(result, null, 2);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
-
+    fetch(gapiUrl, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        const candidates = result.candidates.map(candidate => candidate.content);
+        const formattedResult = candidates.join('\n');
+        console.log(formattedResult);
+        document.getElementById("txtOutput").innerHTML += "Eva: " + formattedResult;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 }
