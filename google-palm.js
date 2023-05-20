28a6f58f8f5569112e6ff411b27410ec3573def3
Steven <servingbaby@users.noreply.github.com>
2023-05-20 12:36:38 -0500

    Update google-palm.js

--- google-palm.js
+++ google-palm.js
@@ -41,7 +41,7 @@ function palmSend() {
       body: JSON.stringify({
         prompt: {
           context:
-            "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, and helpful responses to questions, while being honest and straightforward.",
+            "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, and helpful responses to questions, while being honest and straightforward. " + dateContents,
           examples: [],
           messages: palmMessages.concat([{ author: "0", content: sQuestion }])
         },
