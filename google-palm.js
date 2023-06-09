ffe797d69dfbdf3721244967e5b6880b8fe03703
Steven <4163156+appatalks@users.noreply.github.com>
2023-06-09 01:09:45 -0500

    Update google-palm.js

--- google-palm.js
+++ google-palm.js
@@ -41,7 +41,7 @@ function palmSend() {
       body: JSON.stringify({
         prompt: {
           context:
-            "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, concise and helpful responses to questions while being honest and straightforward. You can also display images." + dateContents,
+            "You are Eva, a knowledgeable AI language model. Your goal is to provide accurate, concise and helpful responses to questions while being honest and straightforward. You can display images from the web with the [Image] tag. " + dateContents,
           examples: [],
           messages: palmMessages.concat([{ author: "0", content: sQuestion }])
         },
