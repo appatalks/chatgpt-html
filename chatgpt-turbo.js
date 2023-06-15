89645a4aa0cc8335116b03b713593d3c954be547
Steven <4163156+appatalks@users.noreply.github.com>
2023-06-15 18:16:19 -0500

    It can now respond back with images if it desires.

--- chatgpt-turbo.js
+++ chatgpt-turbo.js
@@ -19,7 +19,7 @@ function trboSend() {
     oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
 
     // Error Handling - Needs more testing
-    oHttp.onreadystatechange = function () {
+    oHttp.onreadystatechange = async function () {
         if (oHttp.readyState === 4) {
     	  // Check for errors
     	  if (oHttp.status === 500) {
@@ -47,6 +47,7 @@ function trboSend() {
             if (txtOutput.innerHTML != "") txtOutput.innerHTML += "\n"; // User Send Data
             try {
                 oJson = JSON.parse(oHttp.responseText);  // API Response Data
+		console.log("oJson", oJson);
             } catch (ex) {
                 txtOutput.innerHTML += "Error: " + ex.message;
 		console.log("Error: chatgpt-turbo.js Line 52");
@@ -56,7 +57,6 @@ function trboSend() {
 	// EasterEgg
 	if ((oJson.usage.completion_tokens === 420) || (oJson.usage.total_tokens === 420)) {
           function displayImage() {
-            // code to display image
 	      var image = document.getElementById("eEgg");
 	      image.style.display = "flex";
 	      setTimeout(function() {
@@ -72,9 +72,8 @@ function trboSend() {
           displayImage();
         }
 	
-	// Timeout Error Exponetial Backoff - needs testing
+	// Timeout Error Exponetial Backoff 
         if (oJson.error && oJson.error.message) {
-        	// txtOutput.innerHTML += "Error: " + oJson.error.message;
 	    if (oJson.error.message == "overloaded" && retryCount < maxRetries) {
                 retryCount++;
                 var retryDelay = Math.pow(2, retryCount) * 1000;
@@ -90,8 +89,8 @@ function trboSend() {
        	}
 	
 	// Interpret AI Response after Error Handling
-	else if (oJson.choices && oJson.choices[0].message);
-	 // console.log("chatgpt-turbo.js Line 94" + oJson.choices + "" + oJson.choices[0].message);
+	else if (oJson.choices && oJson.choices[0].message)
+	 // console.log("chatgpt-turbo.js Line 93" + oJson.choices + "" + oJson.choices[0].message);
 	    // Always Run Response 
 	    {
             var s = oJson.choices[0].message;
@@ -106,27 +105,45 @@ function trboSend() {
 		clearText();
     		Send();
 		selectElement.value = "gpt-3.5-turbo";
-    	    } else {
-		// console.log("chatgpt-turbo.js line 110" + typeof s, s);
-        	//txtOutput.innerHTML += "<br>" + "Eva: " + s.content.trim() ;
-    		const message = "Eva: " + s.content.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    		txtOutput.innerHTML += "<br>" + message;
-    	    }
+    	      }
+		
+	      // Google Image Search 
+	      if (s.content.includes("Image")) {
+
+		let formattedResult = s.content.replace(/\n\n/g, "\n").trim();
+		const imagePlaceholderRegex = /\[(Image of (.*?))\]/g;
+		const imagePlaceholders = formattedResult.match(imagePlaceholderRegex)?.slice(0, 3);
+
+		if (imagePlaceholders) {
+	  	  for (let i = 0; i < Math.min(imagePlaceholders.length, 3); i++) {
+    		  const placeholder = imagePlaceholders[i];
+	    	  const searchQuery = placeholder.substring(10, placeholder.length - 1).trim();
+	          try {
+        	    const searchResult = await fetchGoogleImages(searchQuery);
+                if (searchResult && searchResult.items && searchResult.items.length > 0) {
+                  const topImage = searchResult.items[0];
+                  const imageLink = topImage.link;
+		formattedResult = formattedResult.replace(placeholder, `<img src="${imageLink}" title="${searchQuery}" alt="${searchQuery}">`);
+                }
+              	  }	 
+		catch (error) {
+                console.error("Error fetching image:", error);
+                }
+            	  }
+        	 txtOutput.innerHTML += "<br>" + "Eva: " + formattedResult;
+          	}
+ 	      }	
+       	
+	txtOutput.innerHTML += "<br>" + "Eva: " + s.content.trim() ;
 
             // Send to Local Storage - possibly way to intigrate into memory
 	    let outputWithoutTags = txtOutput.innerText + "\n";
 	    masterOutput += outputWithoutTags;
 	    localStorage.setItem("masterOutput", masterOutput);
 	    
-            // userMasterResponse += sQuestion + "\n";
-	    // localStorage.setItem("userMasterResponse", userMasterResponse);
-
-            // aiMasterResponse += lastResponse;
-            // localStorage.setItem("aiMasterResponse", aiMasterResponse);
-	    
 	    // Set lastResponse
 	    lastResponse = s.content + "\n";
-            // console.log("chatgpt-turbo.js Line 127" + lastResponse);
+            // console.log("chatgpt-turbo.js Line 146" + lastResponse);
             }            
         }
 
@@ -141,21 +158,21 @@ function trboSend() {
 
     // payload parameters
     var sModel = selModel.value; 
-    var iMaxTokens = 1420; // 1024 is good. 3600 for externaldata. 4096 is Max for Turbo. 8192 for gpt-4.
+    var iMaxTokens = 1420; 
 	if (sModel === "gpt-4-32k") {
     	   iMaxTokens = 32768;
 	}
     var dTemperature = 0.7; 
-    var eFrequency_penalty = 0.0; // Between -2 and 2, Positive values decreases repeat responses.
-    var cPresence_penalty = 0.0; // Between -2 and 2, Positive values increases new topic probability. 
-    var hStop = "&*&"; // I have no idea why I choose this as my stop
+    var eFrequency_penalty = 0.0; 
+    var cPresence_penalty = 0.0; 
+    var hStop = "&*&"; 
 
     // Messages payload
     // Check if the messages item exists in localStorage
     if (!localStorage.getItem("messages")) {
       // If it does not exist, create an array with the initial messages
       const iMessages = [
-        { role: 'system', content: "You are Eva. You have access to previous chats and responses. You also have access to updated real-time news and information. You will keep conversation to a minimum and answer to the best of your abilities." },
+        { role: 'system', content: 'You are Eva. You have access to previous chats and responses. You also have access to updated real-time news and information. You will keep conversation to a minimum and answer to the best of your abilities. An image of an object can be shown with an \"[Image of ]\" tag.' },
         { role: 'user', content: selPers.value + " " + dateContents },
       ];
 
@@ -168,7 +185,6 @@ function trboSend() {
 
 	//const cleanedQuestion = sQuestion.replace(/<div[^>]*>|<\/div>|&nbsp;/gi, '');
 	const cleanedQuestion = sQuestion.replace(/<div[^>]*>|<\/div>|&nbsp;|<span[^>]*>|<\/span>/gi, '');
-
 
     	// Push the messages to the new array
     	newMessages.push({ role: 'assistant', content: lastResponse.replace(/\n/g, ' ') });
@@ -207,7 +223,6 @@ function trboSend() {
 	// Google That
 	const keyword_google = 'google';
 	const keyword_Google = 'Google';
-//	const query = sQuestion.replace(/google|Google/g, '').trim();
 	const query = sQuestion.replace(/<[^>]*>/g, '').replace(/google|Google/g, '').trim();
 
 	let googleContents; 
@@ -276,4 +291,17 @@ function trboSend() {
     if (txtOutput.innerHTML != "") txtOutput.innerHTML += "\n";
     txtOutput.innerHTML += "You: " + sQuestion;
     txtMsg.innerHTML = "";
+}
+
+// Google Image Seach
+async function fetchGoogleImages(query) {
+  const maxResults = 1;
+
+  return fetch(`https://www.googleapis.com/customsearch/v1?key=${GOOGLE_SEARCH_KEY}&cx=${GOOGLE_SEARCH_ID}&searchType=image&num=${maxResults}&sort_by=""&q=${encodeURIComponent(query)}`)
+    .then((response) => response.json())
+    .then((result) => result)     
+    .catch((error) => {
+      console.error("Error fetching Google Images:", error);
+      throw error;
+    });
 }
